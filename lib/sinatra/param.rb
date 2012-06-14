fde4f87049e4f385255e3cffffa38a389688561d
Mattt Thompson <m@mattt.me>
2012-06-14 14:04:04 -0700

    Re-indenting to suppress warning

--- lib/sinatra/param.rb
+++ lib/sinatra/param.rb
@@ -25,51 +25,51 @@ module Sinatra
 
     private
 
-      def coerce(param, type, options = {})
-        return nil if param.nil?
-        return Integer(param) if type == Integer
-        return Float(param) if type == Float
-        return String(param) if type == String
-        return Time.parse(param) if type == Time
-        return Date.parse(param) if type == Date
-        return DateTime.parse(param) if type == DateTime
-        return Array(param.split(options[:delimiter] || ",")) if type == Array
-        return Hash[param.split(options[:delimiter] || ",").map{|c| c.split(options[:separator] || ":")}] if type == Hash
-        return ((/(false|f|no|n|0)$/i === param) ? false : (/(true|t|yes|y|1)$/i === param) ? true : nil) if type == TrueClass || type == FalseClass || type == :boolean
-        return nil
-      end
+    def coerce(param, type, options = {})
+      return nil if param.nil?
+      return Integer(param) if type == Integer
+      return Float(param) if type == Float
+      return String(param) if type == String
+      return Time.parse(param) if type == Time
+      return Date.parse(param) if type == Date
+      return DateTime.parse(param) if type == DateTime
+      return Array(param.split(options[:delimiter] || ",")) if type == Array
+      return Hash[param.split(options[:delimiter] || ",").map{|c| c.split(options[:separator] || ":")}] if type == Hash
+      return ((/(false|f|no|n|0)$/i === param) ? false : (/(true|t|yes|y|1)$/i === param) ? true : nil) if type == TrueClass || type == FalseClass || type == :boolean
+      return nil
+    end
 
-      def validate!(param, options)
-        options.each do |key, value|
-          case key
-            when :required
-              raise InvalidParameterError if value && param.nil?
-            when :blank
-              raise InvalidParameterError if !value && case param
-                  when String
-                    !(/\S/ === param)
-                  when Array, Hash
-                    param.empty?
-                  else
-                    param.nil?
+    def validate!(param, options)
+      options.each do |key, value|
+        case key
+          when :required
+            raise InvalidParameterError if value && param.nil?
+          when :blank
+            raise InvalidParameterError if !value && case param
+                when String
+                  !(/\S/ === param)
+                when Array, Hash
+                  param.empty?
+                else
+                  param.nil?
+              end
+          when :is
+            raise InvalidParameterError unless value === param
+          when :in, :within, :range
+            raise InvalidParameterError unless param.nil? || case value
+                when Range
+                  value.include?(param)
+                else
+                  Array(value).include?(param)
                 end
-            when :is
-              raise InvalidParameterError unless value === param
-            when :in, :within, :range
-              raise InvalidParameterError unless param.nil? || case value
-                  when Range
-                    value.include?(param)
-                  else
-                    Array(value).include?(param)
-                  end
-            when :min
-              raise InvalidParameterError unless param.nil? || value <= param
-            when :max
-              raise InvalidParameterError unless param.nil? || value >= param
-          end
+          when :min
+            raise InvalidParameterError unless param.nil? || value <= param
+          when :max
+            raise InvalidParameterError unless param.nil? || value >= param
         end
       end
     end
+  end
 
   helpers Param
 end

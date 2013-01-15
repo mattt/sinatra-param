fb9a41d285dbcf5f72e50612bd102584fced0434
Steve Agalloco <steve.agalloco@gmail.com>
2013-01-15 00:03:40 -0500

    use simplecov when running tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
-require 'bundler/setup'
+unless ENV['CI']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'spec'
+    add_filter '.bundle'
+  end
+end
+
 require 'sinatra/param'
 require 'example/app'

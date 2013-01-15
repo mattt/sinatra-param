7559af9959143d7dd4a236d29aac361769bd8bf0
Steve Agalloco <steve.agalloco@gmail.com>
2013-01-15 09:40:59 -0500

    fix example

--- example/app.rb
+++ example/app.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 require 'json'
 
-require_relative '../sinatra/param'
+require_relative '../lib/sinatra/param'
 
 class App < Sinatra::Base
   helpers Sinatra::Param
@@ -9,32 +9,32 @@ class App < Sinatra::Base
   before do
     content_type :json
   end
-  
+
   # GET /messages
   # GET /messages?sort=name&order=ASC
   get '/messages' do
     param :sort,  String, default: "name"
     param :order, String, in: ["ASC", "DESC"], transform: :upcase, default: "ASC"
-    
+
     {
       sort: params[:sort],
       order: params[:order]
     }.to_json
   end
-  
+
   # GET /messages/1,2,3,4,5
   get '/messages/:ids' do
     param :ids, Array, required: true
-    
+
     {
       ids: params[:ids]
     }.to_json
   end
-  
+
   # POST /messages/1/response
   post '/messages/:id/response' do
     param :message, String, max: 1024, required: true
-    
+
     {
       message: params[:message]
     }.to_json

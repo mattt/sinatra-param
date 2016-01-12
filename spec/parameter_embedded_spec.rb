5ab59122ecf278ecd2c39b5d13184ef88974d888
Jacob Griswold <jacobagriswold@gmail.com>
2016-01-12 15:02:03 -0500

    added another spec to ensure proper error response for embedded params

--- spec/parameter_embedded_spec.rb
+++ spec/parameter_embedded_spec.rb
@@ -14,4 +14,13 @@ describe 'Parameter' do
       expect(response_body['c']['a']).to eq('UPPERCASE')
     end
   end
+
+  it 'raises error message when required embed is missing' do
+    get('/embedded/parameters', {a: {a: 'string'}}) do |response|
+      response_body = JSON.parse(response.body)
+      expect(response_body['errors']).to be_member 'a[b]'
+      expect(response_body['errors']['a[b]']).to eq 'Parameter is required'
+    end
+  end
+
 end
